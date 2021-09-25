188d2744c78afcf8dbe3f43b2b61d4d9c7cbc4a5
Alex Smith <54478329+alexsmith922@users.noreply.github.com>
2021-09-25 19:28:27 +0000

    - added influencer variables - added print values for influencer keys - added guess variable for user guess

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 #import logo art
 from art import logo
 
-#data range = range(0,49)
+import random
+# data range = range(0,49)
 
 #import vs art
 from art import vs
@@ -9,12 +10,29 @@ from art import vs
 #import game data
 from game_data import data
 
-#print data
 print(logo)
+
+random1 = random.randint(0,49)
+random2 = random.randint(0,49)
 
 #pick two instagrammers to compare
 
-  
+influencer_a_name = data[random1]["name"]
+influencer_a_description = data[random1]["description"]
+influencer_a_country = data[random1]["country"]
+
+influencer_b_name = data[random2]["name"]
+influencer_b_description = data[random2]["description"]
+influencer_b_country = data[random2]["country"]
+
+print(f"Compare A: {influencer_a_name}, {influencer_a_description}, from {influencer_a_country}.")
+
+print(vs)
+
+print(f"Compare B: {influencer_b_name}, {influencer_b_description}, from {influencer_b_country}")
+
+guess = input("Who has more followers? Type 'A' or 'B': ")
+ 
 #Check guess against follower count
 
 #Ask user to make a guess
