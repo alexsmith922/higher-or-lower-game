08eb70b15a54489609ed95b29ee077ed9172f136
Alex Smith <54478329+alexsmith922@users.noreply.github.com>
2021-09-25 19:09:08 +0000

    imported information from other files

---
+++ main.py
@@ -0,0 +1,23 @@
+#import logo art
+from art import logo
+
+#data range = range(0,49)
+
+#import vs art
+from art import vs
+
+#import game data
+from game_data import data
+
+#print data
+print(logo)
+
+#pick two instagrammers to compare
+
+  
+#Check guess against follower count
+
+#Ask user to make a guess
+
+#recycle person b to compare new person against
+
