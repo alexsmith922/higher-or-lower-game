fa2cc2058afffabb88a60201c167e1d267692ede
Alex Smith <54478329+alexsmith922@users.noreply.github.com>
2021-09-25 19:32:01 +0000

    - added function for game() - added check_answer()

--- main.py
+++ main.py
@@ -16,26 +16,29 @@ random1 = random.randint(0,49)
 random2 = random.randint(0,49)
 
 #pick two instagrammers to compare
+def game():
+  influencer_a_name = data[random1]["name"]
+  influencer_a_description = data[random1]["description"]
+  influencer_a_country = data[random1]["country"]
+  influencer_a_followers = data[random1]["follower_count"]
 
-influencer_a_name = data[random1]["name"]
-influencer_a_description = data[random1]["description"]
-influencer_a_country = data[random1]["country"]
+  influencer_b_name = data[random2]["name"]
+  influencer_b_description = data[random2]["description"]
+  influencer_b_country = data[random2]["country"]
+  influencer_b_followers = data[random2]["follower_count"]
 
-influencer_b_name = data[random2]["name"]
-influencer_b_description = data[random2]["description"]
-influencer_b_country = data[random2]["country"]
+  print(f"Compare A: {influencer_a_name}, {influencer_a_description}, from {influencer_a_country}.")
 
-print(f"Compare A: {influencer_a_name}, {influencer_a_description}, from {influencer_a_country}.")
+  print(vs)
 
-print(vs)
+  print(f"Compare B: {influencer_b_name}, {influencer_b_description}, from {influencer_b_country}")
+  
+  #Check guess against follower count
+  def check_answer(guess, follower_count):
+    guess = input("Who has more followers? Type 'A' or 'B': ")
+    
 
-print(f"Compare B: {influencer_b_name}, {influencer_b_description}, from {influencer_b_country}")
+  #Ask user to make a guess
 
-guess = input("Who has more followers? Type 'A' or 'B': ")
- 
-#Check guess against follower count
-
-#Ask user to make a guess
-
-#recycle person b to compare new person against
+  #recycle person b to compare new person against
 
